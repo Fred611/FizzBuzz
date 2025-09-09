b1b5928d4279d6b0696e28c6292128b5f084b430
陈枫元 <chenfengyuan@chenfengyuandeMacBook-Pro.local>
2025-09-09 15:22:32 -0400

    add public to multiples

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -5,13 +5,29 @@ public class Multiples {
         System.out.println(multiples(1000, 3,5));
     }
 
-    static int multiples(int n, int a, int b) {
+    public static int multiples(int n, int a, int b) {
         int i = 1;
         int count = 0;
         while ( i < n) {
 
             boolean divisibleBy3 = i % a == 0;
             boolean divisibleBy5 = i % b == 0;
+
+            // Print our appropriate result
+            if (divisibleBy3 || divisibleBy5) {
+                count++;
+            }
+            i++;
+        }
+        return count;
+    }
+    public static int multiples() {
+        int i = 1;
+        int count = 0;
+        while ( i < 1000) {
+
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
 
             // Print our appropriate result
             if (divisibleBy3 || divisibleBy5) {
