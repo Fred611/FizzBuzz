445666671e3e65982b8e611ab4cbbff3ac29a9dd
陈枫元 <chenfengyuan@chenfengyuandeMacBook-Pro.local>
2025-09-09 15:05:22 -0400

    add parameter for multiples

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,12 +2,16 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
+        System.out.println(multiples(1000, 3,5));
+    }
+
+    static int multiples(int n, int a, int b) {
         int i = 1;
         int count = 0;
-        while ( i < 1000) {
+        while ( i < n) {
 
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
+            boolean divisibleBy3 = i % a == 0;
+            boolean divisibleBy5 = i % b == 0;
 
             // Print our appropriate result
             if (divisibleBy3 || divisibleBy5) {
@@ -15,6 +19,6 @@ public class Multiples {
             }
             i++;
         }
-        System.out.println(count);
+        return count;
     }
 }
